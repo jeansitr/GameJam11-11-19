5d42cd38e24e54329f2367dcc15900ef3a441441
Arianne Tremblay <ariannetablette@gmail.com>
2019-10-13 10:42:02 -0400

    son quand pogne portail

--- jam2019/Assets/Scripts/MapScripts/Doorhandler.cs
+++ jam2019/Assets/Scripts/MapScripts/Doorhandler.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class Doorhandler : MonoBehaviour
 {
+    AudioSource audioPrendrePortail;
+    public AudioClip prendrePortail;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioPrendrePortail = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        audioPrendrePortail.PlayOneShot(prendrePortail, 0.7F);
         EditorSceneManager.LoadScene(1);
     }
 }
