ec306b41be67a2dedf666f4a01c44fff19dbf1bc
Jean-Simon Tremblay <jean-simon_1997@hotmail.com>
2019-10-13 02:21:20 -0400

    finished teleporter and teleport on drought

---
+++ jam2019/Assets/Scripts/MapScripts/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public GameObject Target;
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.transform.position = new Vector3(Target.transform.position.x, Target.transform.position.y - (player.transform.Find("Feet").position.y - player.transform.position.y), 0);
+        }
+    }
+}
