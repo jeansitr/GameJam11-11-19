f32602a20bb9f20ee62fe68af1b78d6a1972e434
Jean-Simon Tremblay <jean-simon_1997@hotmail.com>
2019-10-13 14:17:19 -0400

    end n'est pas fini

--- jam2019/Assets/Scripts/LeaderBoardHandler.cs
+++ jam2019/Assets/Scripts/LeaderBoardHandler.cs
@@ -4,26 +4,83 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using Assets.Scripts;
+using UnityEditor.SceneManagement;
 using TMPro;
 
 public class LeaderBoardHandler : MonoBehaviour
 {
-
     public Transform scrollview;
     public GameObject scorePanel;
+    public Text NameText;
     public Button exitBtn;
-
+    private GameController GC;
+    public float joySens = 0.7f;
+    private int activeLettre = 1;
+    private int[] lettres = { 1, 1, 1 };
+    private char[] alphabet = {' ', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E','e','F','f','G','g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z' };
+    float timer = 0;
     private void OnEnable()
     {
         ShowScore();
+        GC = GameObject.FindObjectOfType<GameController>();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.JoystickButton1))
+        timer += Time.deltaTime;
+        if (timer >= .05)
         {
-            exitBtn.onClick.Invoke();
-        }    
+            timer = 0;
+            if (NameText)
+            {
+                if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Joystick1Button1))
+                {
+                    if (activeLettre == lettres.Length)
+                    {
+                        SaveScore();
+                        EditorSceneManager.LoadScene(0);
+                    }
+
+                    activeLettre++;
+                }
+
+                if (Input.GetKeyDown(KeyCode.Joystick1Button2) || Input.GetKeyDown(KeyCode.Joystick1Button3))
+                {
+                    if (activeLettre != 1)
+                    {
+                        activeLettre--;
+                    }
+                }
+
+                if (Input.GetAxis("Vertical") >= joySens)
+                {
+                    lettres[activeLettre]++;
+                    if (lettres[activeLettre] > alphabet.Length - 1)
+                    {
+                        lettres[activeLettre] = 0;
+                    }
+                }
+
+                if (Input.GetAxis("Vertical") <= -joySens)
+                {
+                    lettres[activeLettre]--;
+                    if (lettres[activeLettre] < 0)
+                    {
+                        lettres[activeLettre] = alphabet.Length - 1;
+                    }
+                }
+
+                ShowName();
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.JoystickButton2) || Input.GetKeyDown(KeyCode.JoystickButton3))
+                {
+                    exitBtn.onClick.Invoke();
+                }
+            }
+        }
+        
     }
 
     public void ShowScore()
@@ -61,5 +118,20 @@ public class LeaderBoardHandler : MonoBehaviour
         });
 
         scoreFile.Close();
+    }
+
+    public void ShowName()
+    {
+        string name = "";
+        for(int i = 0; i < lettres.Length; i++)
+        {
+            name += alphabet[lettres[i]] + " ";
+        }
+        NameText.text = name;
+    }
+
+    public void SaveScore()
+    {
+
     }
 }
