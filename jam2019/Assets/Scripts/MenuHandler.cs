944ba9a6770e4d5a0ffbce28cb60bf2c5d51ef66
Jean-Simon Tremblay <jean-simon_1997@hotmail.com>
2019-10-13 03:05:37 -0400

    tweeked thigs a little

---
+++ jam2019/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        EditorSceneManager.LoadScene(0);
+    }
+}
