d2fbc89014b72a2fef3217106d4aa1428ca91e02
francis <francis.tremblay9@hotmail.com>
2019-10-13 10:46:48 -0400

    fix de toute

---
+++ jam2019/Assets/UIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text scoreText;
+    public Text healthText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateScore(int score)
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void updateHealth(int health)
+    {
+        healthText.text = "Health: " + health;
+        Debug.Log("Updated health : " + health);
+    }
+}
