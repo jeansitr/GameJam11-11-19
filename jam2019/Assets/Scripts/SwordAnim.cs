d351d7907f8d8722a7a67d706e7ad56a866aa50e
Arianne Tremblay <ariannetablette@gmail.com>
2019-10-13 15:02:42 -0400

    son quand coup épée

--- jam2019/Assets/Scripts/SwordAnim.cs
+++ jam2019/Assets/Scripts/SwordAnim.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class SwordAnim : MonoBehaviour
 {
     public GameObject sword;
+    AudioSource audioCoupEpee;
+    public AudioClip impact;
+
+    void Start()
+    {
+        audioCoupEpee = GetComponent<AudioSource>();
+    }
+
     public void SwordFinish()
     {
         sword.GetComponent<BoxCollider2D>().enabled = false;
@@ -13,5 +23,6 @@ public class SwordAnim : MonoBehaviour
     public void SwordStart()
     {
         sword.GetComponent<BoxCollider2D>().enabled = true;
+        audioCoupEpee.PlayOneShot(impact, 0.7F);
     }
 }
