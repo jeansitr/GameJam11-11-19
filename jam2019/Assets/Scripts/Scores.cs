3ec1d117f6e74b83883b28e569bf577298b01497
Jean-Simon Tremblay <jean-simon_1997@hotmail.com>
2019-10-13 15:01:14 -0400

    finished scoreboard

---
+++ jam2019/Assets/Scripts/Scores.cs
@@ -0,0 +1,10 @@
+﻿using SQLite4Unity3d;
+
+[Table("Scores")]
+public class Scores
+{
+    [PrimaryKey, AutoIncrement]
+    public int id { get; set; }
+    public string name { get; set; }
+    public string score { get; set; }
+}
