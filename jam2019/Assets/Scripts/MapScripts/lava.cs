3dfcda89a6262db3096136122ff63154c2a2acee
Jean-Simon Tremblay <jean-simon_1997@hotmail.com>
2019-10-13 01:07:59 -0400

    spawn lava world

---
+++ jam2019/Assets/Scripts/MapScripts/lava.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lava : MonoBehaviour
+{
+    public Transform[] possibleSpawn = new Transform[11];
+    public GameObject civil;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        System.Random rdn = new System.Random();
+        List<int> selected = new List<int>();
+        for (int i = 0; i < 3; i++)
+        {
+            int e;
+            do
+            {
+                e = rdn.Next(0, 11);
+            }
+            while (selected.Contains(e));
+
+            selected.Add(e);
+
+            GameObject civilInstance = Instantiate(civil, transform);
+            Transform pos = possibleSpawn[e];
+            civilInstance.transform.position = new Vector3(pos.position.x, pos.position.y - (civilInstance.transform.Find("Feet").position.y - civilInstance.transform.position.y), 0);
+        }
+    }
+}
